03a1ad795d38eff4c2b86c1d2822351c0201315b
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-11 15:12:13 +0200

    creation

--- index.php
+++ index.php
@@ -6,40 +6,39 @@
         <title>PHP ex 7.5 Formulaire: Client & Server</title>
     </head>
     <body >
-        <!--
-            Créer un formulaire sur la page index.php avec :  
+        <?php
+        /*  Créer un formulaire sur la page index.php avec :  
                 Une liste déroulante pour la civilité (Mr ou Mme)
                 Un champ texte pour le nom
                 Un champ texte pour le prénom  
             Ce formulaire doit rediriger vers la page index.php. 
             Vous avez le choix de la méthode.
-        -->
-        <?php
-        if (isset($_REQUEST['civil']) && isset($_REQUEST['userName']) && isset($_REQUEST['userFirstname']))
-        {
-            // Server part
-            $civilInfo=$_REQUEST['civil'];
-            $userName=$_REQUEST['userName'];
-            $userFirstname=$_REQUEST['userFirstname'];
-            echo ("Server: ".$civilInfo." ".$userName." ".$userFirstname);
-        }
-        else
+        */
+        if ((empty($_REQUEST['civil'])) || (empty($_REQUEST['userName'])) || (empty($_REQUEST['userFirstname'])))
         {
             // Client part
         ?>
         <form action="index.php" method="get">
-            <label for="civilId">Civilité:</label>
+            <label>Client: </label>
             <select name="civil" id="civilId">
                 <option value="Mr">Mr</option>    			
                 <option value="Mme">Mme</option>   
             </select>
             <label for="userName">Nom: </label>
-	    	<input type="string" name="userName" placeholder="nom">
+        	<input type="string" name="userName" placeholder="nom">
             <label for="userFirstname">Prénom: </label>
-    		<input type="string" name="userFirstname" placeholder="prénom">            
+        	<input type="string" name="userFirstname" placeholder="prénom">            
             <input type="submit" value="OK">
         </form>
         <?php
+        }
+        else
+        {
+            // Server part
+            $civilInfo= $_REQUEST['civil'];
+            $userName= $_REQUEST['userName'];
+            $userFirstname= $_REQUEST['userFirstname'];
+            echo ("Server: ".$civilInfo." ".$userName." ".$userFirstname);
         }
         ?>
     </body>
